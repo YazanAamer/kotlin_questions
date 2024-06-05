e9df285957a4decda203e48a67c1a33fd7e2bd68
Omar Ibrahim <omarom.ibra@outlook.com>
2024-06-05 13:27:54 +0400

    updated exercise 14 (coroutines)

--- src/main/kotlin/14-coroutines.kt
+++ src/main/kotlin/14-coroutines.kt
@@ -1,11 +1,12 @@
 import kotlinx.coroutines.*
 
-suspend fun main() {
-    coroutineScope {launch {
+ suspend fun main() {
+    val job = GlobalScope.launch (){
         for (x in 1..5) {
             delay(1000L)
             println(x)
         }
-    }}
+    }
     println("Hello, world!")
-}
+    job.join()
+ }
