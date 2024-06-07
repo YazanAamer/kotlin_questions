f5d5f75cc986e34dee3f5e431c57e6bc1a5c1626
Omar Ibrahim <omarom.ibra@outlook.com>
2024-06-07 11:46:42 +0400

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,24 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    val lifecycle_version = "2.8.1"
+    val arch_version = "2.2.0"
+
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(19)
 }
