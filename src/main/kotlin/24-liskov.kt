b3d5f27db9c3ee00ae7e6354cd90618f6c756a12
Omar Ibrahim <omarom.ibra@outlook.com>
2024-06-10 13:20:05 +0400

    exercise 24 liskov

---
+++ src/main/kotlin/24-liskov.kt
@@ -0,0 +1,23 @@
+// Liskov Substitution Principle: everything that applies to a base class should apply to all its children
+//                              i.e. if a base class reference points to a child, all its methods should work
+
+abstract class Shape2(var p1: Double, var p2: Double, var p3: Double) {
+    abstract fun calculateArea(): Double
+}
+
+class Square2(p1: Double, p2: Double, p3: Double): Shape2(p1, p2, p3){
+
+    override fun calculateArea(): Double {return this.p1 * this.p1}
+}
+
+class Rectangle2(p1: Double, p2: Double, p3: Double): Shape2(p1, p2, p3){
+
+    override fun calculateArea(): Double {return this.p1 * this.p2}
+}
+
+fun main() {
+    val mySquare = Square2(1.0, 2.0, 3.0)
+    val myRectangle = Rectangle2(1.0, 2.0, 3.0)
+    println(mySquare.calculateArea())
+    println(myRectangle.calculateArea())
+}
